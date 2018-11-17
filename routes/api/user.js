bc696d4fbdd3d75e34d7481a34ba208a5104ed88
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-11-17 16:30:38 +0100

    create current user route

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,6 +4,7 @@ const { validationResult } = require("express-validator/check");
 const validation = require("../../validation/userValidation");
 const router = express.Router();
 const User = require("../../models/User");
+const authUser = require("../../middlewares/login");
 
 router.post("/signup", validation.signup, async (req, res) => {
   const { username, email, password } = req.body;
@@ -37,6 +38,19 @@ router.post("/login", validation.login, async (req, res, next) => {
     if (user) {
       req.session.isLoggedIn = true;
       req.session.user = user;
+      return res.status(200).json(user);
+    }
+  } catch (e) {
+    const error = new Error(e);
+    error.httpStateCode = 500;
+    return next(error);
+  }
+});
+
+router.get("/current-user", authUser, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.user.email });
+    if (user) {
       return res.status(200).json(user);
     }
   } catch (e) {
