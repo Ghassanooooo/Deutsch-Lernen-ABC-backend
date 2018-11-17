ca1a958129a8f65062fc498feaf0f52f02ec5ab3
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-11-17 21:50:09 +0100

    added check for header in login middleware

--- middlewares/login.js
+++ middlewares/login.js
@@ -3,6 +3,11 @@ const keys = require("../config/keys");
 
 module.exports = (req, res, next) => {
   const token = req.get("Authorization");
+  if (!token) {
+    const error = new Error("Not Authorization!");
+    error.statusCode = 401;
+    throw error;
+  }
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, keys.jwtToken);
